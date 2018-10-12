c9fb73a74a88f5613f1097573bb9551862c37ba7
Milton Gonzalez <mgonzalez@ximil.co>
2018-10-11 22:54:25 -0700

    Added Asyncstorage to storage photos locally

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, StyleSheet, Text, View, Image } from 'react-native';
+import { ActivityIndicator, AsyncStorage, Alert, Button, FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
 import ImageOverlay from "react-native-image-overlay";
 
 export default class App extends React.Component {
@@ -9,24 +10,49 @@ export default class App extends React.Component {
     this.state ={ isLoading: true}
   }
 
-  componentDidMount(){
-    return fetch('http://jsonplaceholder.typicode.com/photos')
-      .then((response) => response.json())
-      .then((responseJson) => {
+  async componentDidMount(){
 
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson,
-        }, function(){
+    //we validate if photos storage exists
+    const photoStorage = await AsyncStorage.getItem('Photos');
 
+
+    //If its not set we need to fetch API and saved results
+    if(!photoStorage) {
+
+      return fetch('http://jsonplaceholder.typicode.com/photos')//Fetch API endpoint
+        .then((response) => response.json())
+        .then((responseJson) => {
+
+          this.setState({
+            isLoading: false,//stop loading
+            dataSource: responseJson,
+          }, async function(){
+                const photoData = await JSON.stringify(responseJson);
+                await AsyncStorage.setItem('Photos', photoData);
+                console.log('saved');
+          });
+
+        })
+        .catch((error) =>{
+          console.error(error);
         });
 
-      })
-      .catch((error) =>{
-        console.error(error);
+    }else{//Photos storage exists, so we get the data
+
+      const photos = JSON.parse(await AsyncStorage.getItem('Photos'));
+
+      this.setState({
+        isLoading: false,//stop loading
+        dataSource: photos,
       });
+
+    }
+
   }
 
+  _orderImages() {
+  Alert.alert('on Press!');
+ }
 
   render() {
     if(this.state.isLoading){
@@ -38,18 +64,27 @@ export default class App extends React.Component {
     }
 
     return(
-      <View style={{flex: 1, flexDirection: 'row', paddingTop:40}}>
+      <View style={{flex: 1,  paddingTop:40}}>
+
         <FlatList
           horizontal={true}
           data={this.state.dataSource}
-          renderItem={({item}) => <ImageOverlay rounded={5} containerStyle={{borderWidth:2, borderColor:'#000', width:100 }}  source={{uri: item.url }}
+          renderItem={({item}) => <ImageOverlay rounded={10} containerStyle={{borderWidth:2, borderColor:'#000', width:100, marginHorizontal: 10 }}  source={{uri: item.url }}
                                     title={item.title} height={100} titleStyle={{  transform: [{ rotate: '330deg'}] }} />}
           keyExtractor={({id}, index) => id}
         />
+
+        <Button
+          onPress={this._orderImages}
+          title="Reorder"
+          color="#841584"
+          accessibilityLabel="Reorder images"
+        />
+
       </View>
+
     );
   }
-
 
 }
 
